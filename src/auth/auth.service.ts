eee6a0376b11365303c321ed002f57690bb49d4f
Jason Jones <jason.jones@act.com>
2022-08-08 14:12:59 -0700

    basic controller service call

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable({})
 export class AuthService{
 
-  login() {
-    return 'hi'
-  } 
+
   
-  signup() { return 'hi' }
+  signup() { return {msg: 'I have signed up'} }
+
+
+  signin() {
+    return { msg: 'I have signed in '}
+  }
 
 }
