78c58d308f70b3929ac1e487150be3f6c531d7be
Jason Jones <jason.jones@act.com>
2022-08-08 13:56:41 -0700

    working very basic

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService{
+
+  login() {
+    return 'hi'
+  } 
+  
+  signup() { return 'hi' }
+
+}
