3dbe0c22e9d58f02088d46127d62fdfac9bffd93
Jason Jones <jason.jones@crystalinventory.io>
2022-08-10 13:06:16 -0700

    Save user using select for return

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true           // strips out anything extra fro the request
+  }))
   await app.listen(3333);
 }
 bootstrap();
