588be51e8aeb09d3aec9b1ea93c39162b5426698
Jason Jones <jason.jones@crystalinventory.io>
2022-08-10 13:31:16 -0700

    Signup done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class AuthService{
@@ -11,23 +13,25 @@ export class AuthService{
 
   async signup(dto: AuthDto) { 
     const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash
-      },
-      /*
-      select: {
-        id: true,
-        email: true,
-        createdAt: true
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash
+        },
+      });
+      delete user.hash;   //Removes only the hash instead of the big select
+      return user;
+  
+    } catch(error) {
+      if(error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already used');
+        }
       }
-      */
-    });
-
-    delete user.hash;   //Removes only the hash instead of the big select
-
-    return user;
+      throw error;
+    }
   }
 
 
