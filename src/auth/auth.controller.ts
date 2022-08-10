43a1b97b7496ff00443c7f12518eeedd90460d37
Jason Jones <jason.jones@crystalinventory.io>
2022-08-10 09:01:38 -0700

    Some good destructuring of request parms

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(
+    @Body('email') email:string, 
+    @Body('password', ParseIntPipe) password: string
+  ) {
+    console.log({
+      email,
+      typeOfEmail: typeof email, 
+      password,
+      typeOfPassword: typeof password
+    })
     return this.authService.signup()
   }
 
